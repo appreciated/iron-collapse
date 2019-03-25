3eb7384d863118f05e42c3e99eb5a13f69a3d0bf
Johannes Goebel <GoebelJohannes@gmx.net>
2019-03-25 12:41:55 +0100

    fixed some styling related issues with IronCollapseLayout

--- src/test/java/com/github/appreciated/DemoView.java
+++ src/test/java/com/github/appreciated/DemoView.java
@@ -1,21 +1,23 @@
 package com.github.appreciated;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route("")
 public class DemoView extends Div {
 
     public DemoView() {
-        addServerSideToggle();
-        addClientSideToggle();
+        add(addServerSideToggle());
+        add(addClientSideToggle());
     }
 
-    private void addServerSideToggle() {
+    private Component addServerSideToggle() {
         Label label = new Label("üpasidfjgkljasdklfgjklsjdfklgöjKLSHDFÖHüpasidfjgkljasdklfgjklsjdfklgöjKLSHDFÖHüpasidfjgkljasdklfgjklsjdfklgöjKLSHDFÖHüpasidfjgkljasdklfgjklsjdfklgöjKLSHDFÖHüpasidfjgkljasdklfgjklsjdfklgöjKLSHDFÖHüpasidfjgkljasdklfgjklsjdfklgöjKLSHDFÖH");
         label.setWidth("100%");
         label.getStyle().set("word-wrap", "break-word");
@@ -25,18 +27,17 @@ public class DemoView extends Div {
         content.setMargin(true);
         content.getStyle().set("box-shadow", "0 0 5px rgba(0,0,0,0.5)");
         IronCollapse collapse = new IronCollapse(content);
-        add(new H2("Serverside Toggle"), new Button("Collapse", buttonClickEvent -> collapse.toggle()), collapse);
+        return new VerticalLayout(new H2("Serverside Toggle"), new Button("Collapse", buttonClickEvent -> collapse.toggle()), collapse);
     }
 
-    private void addClientSideToggle() {
+    private Component addClientSideToggle() {
         Label label = new Label("üpasidfjgkljasdklfgjklsjdfklgöjKLSHDFÖHüpasidfjgkljasdklfgjklsjdfklgöjKLSHDFÖHüpasidfjgkljasdklfgjklsjdfklgöjKLSHDFÖHüpasidfjgkljasdklfgjklsjdfklgöjKLSHDFÖHüpasidfjgkljasdklfgjklsjdfklgöjKLSHDFÖHüpasidfjgkljasdklfgjklsjdfklgöjKLSHDFÖH");
         label.setWidth("100%");
         label.getStyle().set("word-wrap", "break-word");
-        HorizontalLayout content = new HorizontalLayout(label);
+        VerticalLayout content = new VerticalLayout(label, addServerSideToggle());
         content.setWidth("300px");
-        content.setHeight("300px");
         content.setMargin(true);
         content.getStyle().set("box-shadow", "0 0 5px rgba(0,0,0,0.5)");
-        add(new H2("Client Side Toggle"), new IronCollapseLayout(new Button("Collapse"), content));
+        return new VerticalLayout(new H2("Client Side Toggle"), new IronCollapseLayout(new Button("Collapse"), content));
     }
 }
